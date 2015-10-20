35a90d63a1e6863dca2aa705a811dee6f2b96293
Eric <khrylx@gmail.com>
2015-10-20 13:17:28 -0400

    Fixed a bug in BVH construction.

--- src/bvh.cpp
+++ src/bvh.cpp
@@ -6,6 +6,8 @@
 #include <iostream>
 #include <stack>
 
+#define MAX_RECURSIVE_DEPTH 5000
+
 using namespace std;
 
 namespace CMU462 { namespace StaticScene {
@@ -17,131 +19,143 @@ struct Bucket{
 };
 
 // buildBVH helper
-void buildBVH(vector<Primitive *>& primitives, BVHNode* node, int bucketNum, size_t max_leaf_size)
+void buildBVH(vector<Primitive *>& primitives, BVHNode* node, int bucketNum, size_t max_leaf_size, int depth)
 {
-    
-    double minC[3] = {INF_D, INF_D, INF_D};
-    int minBIndex[3];
-    
-    for (int k = 0; k < 3; k++) {
-        double ub = node->bb.max[k];
-        double lb = node->bb.min[k];
-        if (ub == lb) {
-            continue;
-        }
-        double interval = (ub-lb) / bucketNum;
-        vector<Bucket> Buckets(bucketNum);
-        vector<Bucket> rBuckets(bucketNum);
-        
-        // calculate buckets
-        for (int i = 0; i < node->range; i++) {
-            Primitive* P = primitives[node->start + i];
-            BBox pbb = P->get_bbox();
-            double c = (pbb.min[k] + pbb.max[k])*0.5;
-            int bIndex = (c - lb) / interval;
-            //cout << bIndex <<endl;
-            Buckets[bIndex].bb.expand(pbb);
-            Buckets[bIndex].prim_count++;
-        }
-
-        // expand bucket reversely
-        for (int i = 0; i < bucketNum; i++) {
-            rBuckets[i] = Buckets[bucketNum-i-1];
-            if (i > 0) {
-                rBuckets[i].bb.expand(rBuckets[i-1].bb);
-                rBuckets[i].prim_count += rBuckets[i-1].prim_count;
-            }
-        }
-        
-        
-        // expand bucket sequentially
-        for (int i = 1; i < bucketNum; i++) {
-            Buckets[i].bb.expand(Buckets[i-1].bb);
-            Buckets[i].prim_count += Buckets[i-1].prim_count;
-        }
-        
-        // calculate partition reaching minimal cost
-        for (int i = 0; i < bucketNum-1; i++) {
-            Bucket& b1 = Buckets[i];
-            Bucket& b2 = rBuckets[bucketNum-i-2];
-            double C = (b1.bb.extent.x * b1.bb.extent.y + b1.bb.extent.x * b1.bb.extent.z + b1.bb.extent.y * b1.bb.extent.z) * b1.prim_count
-            +(b2.bb.extent.x * b2.bb.extent.y + b2.bb.extent.x * b2.bb.extent.z + b2.bb.extent.y * b2.bb.extent.z) * b2.prim_count;
-            if (C < minC[k]) {
-                minC[k] = C;
-                minBIndex[k] = i+1;
-            }
-        }
+    if (depth > MAX_RECURSIVE_DEPTH) {
+        node->l = NULL;
+        node->r = NULL;
+        return;
     }
     
-    // select best axis
-    int axis = 0;
-    double cost = minC[0];
-    for (int i = 1; i < 3; i++) {
-        if (minC[i] < cost) {
-            axis = i;
-            cost = minC[i];
-        }
-    }
-    
-    // partition primitives by the partition line computed using quicksort-like algorithm.
-    double ub = node->bb.max[axis];
-    double lb = node->bb.min[axis];
-    double pLine = lb + (ub-lb)*minBIndex[axis]/bucketNum;
-    
-    int i = node->start - 1;
-    int j = node->start + node->range;
-    BBox bb1,bb2;
-
-    while (i < j) {
-        do{
-            i++;
-            if (i >= (int)(node->start + node->range)) {
-                break;
-            }
-            bb1 = primitives[i]->get_bbox();
-        }while((bb1.min[axis] + bb1.max[axis])*0.5 < pLine);
-        
-        do{
-            j--;
-            if (j < (int)node->start) {
-                break;
-            }
-            bb2 = primitives[j]->get_bbox();
-        }while((bb2.min[axis] + bb2.max[axis])*0.5 > pLine);
-        
-        if (i < j) {
-            Primitive* tmp = primitives[i];
-            primitives[i] = primitives[j];
-            primitives[j] = tmp;
-        }
-        else
-            break;
-    }
-    
-    // Recalculate bounding box
     BBox lbb,rbb;
-    int lRange = i - node->start;
-    int rRange = node->range - lRange;
+    int lRange,rRange;
     
-    for (int j = 0; j < node->range; j++) {
-        bb1 = primitives[node->start + j]->get_bbox();
-        if (j < lRange) {
-            lbb.expand(bb1);
+    // Keep less local variables to allow more recursion.
+    {
+        double minC[3] = {INF_D, INF_D, INF_D};
+        int minBIndex[3];
+        
+        for (int k = 0; k < 3; k++) {
+            double ub = node->bb.max[k];
+            double lb = node->bb.min[k];
+            if (ub == lb) {
+                continue;
+            }
+            double interval = (ub-lb) / bucketNum;
+            vector<Bucket> Buckets(bucketNum);
+            vector<Bucket> rBuckets(bucketNum);
+            
+            // calculate buckets
+            for (int i = 0; i < node->range; i++) {
+                Primitive* P = primitives[node->start + i];
+                BBox pbb = P->get_bbox();
+                double c = (pbb.min[k] + pbb.max[k])*0.5;
+                int bIndex = (c - lb) / interval;
+                //cout << bIndex <<endl;
+                Buckets[bIndex].bb.expand(pbb);
+                Buckets[bIndex].prim_count++;
+            }
+            
+            // expand bucket reversely
+            for (int i = 0; i < bucketNum; i++) {
+                rBuckets[i] = Buckets[bucketNum-i-1];
+                if (i > 0) {
+                    rBuckets[i].bb.expand(rBuckets[i-1].bb);
+                    rBuckets[i].prim_count += rBuckets[i-1].prim_count;
+                }
+            }
+            
+            
+            // expand bucket sequentially
+            for (int i = 1; i < bucketNum; i++) {
+                Buckets[i].bb.expand(Buckets[i-1].bb);
+                Buckets[i].prim_count += Buckets[i-1].prim_count;
+            }
+            
+            // calculate partition reaching minimal cost
+            for (int i = 0; i < bucketNum-1; i++) {
+                Bucket& b1 = Buckets[i];
+                Bucket& b2 = rBuckets[bucketNum-i-2];
+                double C = (b1.bb.extent.x * b1.bb.extent.y + b1.bb.extent.x * b1.bb.extent.z + b1.bb.extent.y * b1.bb.extent.z) * b1.prim_count
+                +(b2.bb.extent.x * b2.bb.extent.y + b2.bb.extent.x * b2.bb.extent.z + b2.bb.extent.y * b2.bb.extent.z) * b2.prim_count;
+                if (C < minC[k]) {
+                    minC[k] = C;
+                    minBIndex[k] = i+1;
+                }
+            }
         }
-        else{
-            rbb.expand(bb1);
+        
+        // select best axis
+        int axis = 0;
+        double cost = minC[0];
+        for (int i = 1; i < 3; i++) {
+            if (minC[i] < cost) {
+                axis = i;
+                cost = minC[i];
+            }
+        }
+        
+        // partition primitives by the partition line computed using quicksort-like algorithm.
+        double ub = node->bb.max[axis];
+        double lb = node->bb.min[axis];
+        double pLine = lb + (ub-lb)*minBIndex[axis]/bucketNum;
+        
+        int i = node->start - 1;
+        int j = node->start + node->range;
+        BBox bb1,bb2;
+        
+        while (i < j) {
+            do{
+                i++;
+                if (i >= (int)(node->start + node->range)) {
+                    break;
+                }
+                bb1 = primitives[i]->get_bbox();
+            }while((bb1.min[axis] + bb1.max[axis])*0.5 < pLine);
+            
+            do{
+                j--;
+                if (j < (int)node->start) {
+                    break;
+                }
+                bb2 = primitives[j]->get_bbox();
+            }while((bb2.min[axis] + bb2.max[axis])*0.5 > pLine);
+            
+            if (i < j) {
+                Primitive* tmp = primitives[i];
+                primitives[i] = primitives[j];
+                primitives[j] = tmp;
+            }
+            else
+                break;
+        }
+        
+        // Recalculate bounding box
+        lRange = i - node->start;
+        rRange = node->range - lRange;
+        
+        for (int j = 0; j < node->range; j++) {
+            bb1 = primitives[node->start + j]->get_bbox();
+            if (j < lRange) {
+                lbb.expand(bb1);
+            }
+            else{
+                rbb.expand(bb1);
+            }
         }
     }
+    
     
     node->l = lRange == 0 ? NULL : new BVHNode(lbb, node->start, lRange);
     node->r = rRange == 0 ? NULL : new BVHNode(rbb, node->start + lRange, rRange);
     
-    if (lRange > max_leaf_size) {
-        buildBVH(primitives, node->l, bucketNum, max_leaf_size);
+    
+    if (lRange <= max_leaf_size || rRange <= max_leaf_size) {
+        return;
     }
-    if (rRange > max_leaf_size) {
-        buildBVH(primitives, node->r, bucketNum, max_leaf_size);
-    }
+    
+    buildBVH(primitives, node->l, bucketNum, max_leaf_size, depth+1);
+    buildBVH(primitives, node->r, bucketNum, max_leaf_size, depth+1);
     
     return;
     
@@ -167,7 +181,7 @@ BVHAccel::BVHAccel(const std::vector<Primitive *> &_primitives,
   root = new BVHNode(bb, 0, primitives.size());
 
     
-    buildBVH(primitives, root, 16, max_leaf_size);
+    buildBVH(primitives, root, 16, max_leaf_size, 0);
 }
 
 //  destroy BVH nodesv
