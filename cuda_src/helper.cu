0a7e728e1f31c9f54feadcc3aa734415979ac505
Eric <khrylx@gmail.com>
2016-04-14 20:26:46 -0400

    use separate files.

--- cuda_src/helper.cu
+++ cuda_src/helper.cu
@@ -4,6 +4,13 @@
 #include <cuda_runtime.h>
 #define Pi 3.1415926
 
+__device__ float2 gridSampler(curandState *s) {
+    float2 rt;
+    rt.x = curand_uniform(s);
+    rt.y = curand_uniform(s);
+    return rt;
+}
+
 __device__ inline float
 power(float X,float Y)
 {
@@ -32,15 +39,31 @@ normalize3D(float *X)
 }
 
 __device__ inline void
-initVector3D(float x, float y, float z, float* S)
+scaleVector3D(float* X, float a)
 {
-    S[0] = x;
-    S[1] = y;
-    S[2] = z;
+    X[0] *= a;
+    X[1] *= a;
+    X[2] *= a;
+}
+
+// S = X + a * Y
+__device__ inline void
+addScaledVector3D(float* X, float* Y, float a, float* S)
+{
+    S[0] = X[0] + Y[0] * a;
+    S[1] = X[1] + Y[1] * a;
+    S[2] = X[2] + Y[2] * a;
 }
 
 __device__ inline void
-readVector3D(float* src, float *dst) {
+readVector3D(float x, float y, float z, float *dst) {
+    dst[0] = x;
+    dst[1] = y;
+    dst[2] = z;
+}
+
+__device__ inline void
+readVector3D(const float* src, float *dst) {
     dst[0] = src[0];
     dst[1] = src[1];
     dst[2] = src[2];
@@ -190,10 +213,104 @@ void MatrixScale3D(float* X,float a)
     }
 }
 
+inline __device__
+void MatrixTranspose3D(float* X,float* S)
+{
+    for(int k = 0; k < 9; k++)
+    {
+        S[(k % 3) * 3 + k / 3] = X[k];
+    }
+}
 
+inline __device__
+void make_coord_space(const float* n, float* o2w) {
+
+    float z[3];
+    float h[3];
+    float x[3];
+    float y[3];
+
+    readVector3D(n, z);
+    readVector3D(z, h);
+
+    if (fabs(h[0]) <= fabs(h[1]) && fabs(h[0]) <= fabs(h[2])) h[0] = 1.0;
+    else if (fabs(h[1]) <= fabs(h[0]) && fabs(h[1]) <= fabs(h[2])) h[1] = 1.0;
+    else h[2] = 1.0;
+
+    normalize3D(z);
+    VectorCross3D(h, z, y);
+    normalize3D(y);
+    VectorCross3D(z, y, x);
+    normalize3D(x);
+
+    o2w[0] = x[0]; o2w[1] = y[0]; o2w[2] = z[0];
+    o2w[3] = x[1]; o2w[4] = y[1]; o2w[5] = z[1];
+    o2w[6] = x[2]; o2w[7] = y[2]; o2w[8] = z[2];
+}
 
 inline __device__ void
 gpuAdd(float *A, float *B, float *C)
 {
     *C = *A + *B;
+}
+
+__global__ void
+vectorAdd(float *A, float *B, float *C, int numElements)
+{
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+
+    if (i < numElements)
+    {
+        gpuAdd(A + i, B + i, C + i);
+        //C[i] = A[i] + B[i];
+    }
+}
+
+
+__global__ void
+printInfo()
+{
+    GPUBSDF* bsdfs = const_bsdfs;
+    GPUCamera camera = const_camera;
+
+    for (int i = 0; i < 8; i++) {
+        if (bsdfs[i].type == 0) {
+            printf("0: %lf %lf %lf\n", bsdfs[i].albedo[0], bsdfs[i].albedo[1], bsdfs[i].albedo[2] );
+        }
+        else if (bsdfs[i].type == 1) {
+            printf("1: %lf %lf %lf\n", bsdfs[i].reflectance[0], bsdfs[i].reflectance[1], bsdfs[i].reflectance[2] );
+        }
+        else if (bsdfs[i].type == 2) {
+            //cout << "2" << endl;
+        }
+        else if (bsdfs[i].type == 3) {
+            printf("3: %lf %lf %lf\n", bsdfs[i].reflectance[0], bsdfs[i].reflectance[1], bsdfs[i].reflectance[2] );
+            printf("3: %lf %lf %lf\n", bsdfs[i].transmittance[0], bsdfs[i].transmittance[1], bsdfs[i].transmittance[2] );
+        }
+        else {
+            printf("4: %lf %lf %lf\n", bsdfs[i].albedo[0], bsdfs[i].albedo[1], bsdfs[i].albedo[2] );
+        }
+    }
+
+
+    printf("%lf %lf %lf\n", camera.pos[0], camera.pos[1], camera.pos[2] );
+
+
+    float* positions = const_params.positions;
+    float* normals = const_params.normals;
+
+    printf("+++++++++++++++++++++++\n");
+    for (int i = 0; i < const_params.primNum; i++) {
+        printf("%d %d %d\n\n",const_params.types[i] ,const_params.bsdfIndexes[i], const_bsdfs[const_params.bsdfIndexes[i]].type);
+
+        printf("%lf %lf %lf\n", positions[9 * i], positions[9 * i + 1], positions[9 * i + 2] );
+        printf("%lf %lf %lf\n", positions[9 * i + 3], positions[9 * i + 4], positions[9 * i + 5] );
+        printf("%lf %lf %lf\n", positions[9 * i + 6], positions[9 * i + 7], positions[9 * i + 8] );
+        printf("=======================\n");
+        printf("%lf %lf %lf\n", normals[9 * i], normals[9 * i + 1], normals[9 * i + 2] );
+        printf("%lf %lf %lf\n", normals[9 * i + 3], normals[9 * i + 4], normals[9 * i + 5] );
+        printf("%lf %lf %lf\n", normals[9 * i + 6], normals[9 * i + 7], normals[9 * i + 8] );
+        printf("+++++++++++++++++++++++\n\n");
+    }
+
 }
---
+++ cuda_src/intersect.cu
@@ -0,0 +1,173 @@
+// primitive and normals are shift pointers to the primitive and normal we selected
+__device__ bool triangleIntersect(int primIndex, GPURay& r) {
+
+    float* primitive = const_params.positions + 9 * primIndex;
+
+    float* v1 = primitive;
+    float* v2 = primitive + 3;
+    float* v3 = primitive + 6;
+
+    float e1[3], e2[3], s[3];
+    subVector3D(v2, v1, e1);
+    subVector3D(v3, v1, e2);
+    subVector3D(r.o, v1, s);
+
+    float tmp[3];
+    VectorCross3D(e1, r.d, tmp);
+    double f = VectorDot3D(tmp, e2);
+    if (f == 0) {
+        return false;
+    }
+
+    VectorCross3D(s, r.d, tmp);
+    double u = VectorDot3D(tmp, e2) / f;
+    VectorCross3D(e1, r.d, tmp);
+    double v = VectorDot3D(tmp, s) / f;
+    VectorCross3D(e1, s, tmp);
+    double t = - VectorDot3D(tmp, e2) / f;
+
+    if (u >= 0 && v >= 0 && u+v <= 1 && t > r.min_t && t < r.max_t) {
+        return true;
+    }
+
+    return false;
+}
+
+// primitive and normals are shift pointers to the primitive and normal we selected
+__device__ bool triangleIntersect(int primIndex, GPURay& r, GPUIntersection *isect) {
+
+    float* primitive = const_params.positions + 9 * primIndex;
+    float* normals = const_params.normals + 9 * primIndex;
+
+    float* v1 = primitive;
+    float* v2 = primitive + 3;
+    float* v3 = primitive + 6;
+
+    float e1[3], e2[3], s[3];
+    subVector3D(v2, v1, e1);
+    subVector3D(v3, v1, e2);
+    subVector3D(r.o, v1, s);
+
+    float tmp[3];
+    VectorCross3D(e1, r.d, tmp);
+    double f = VectorDot3D(tmp, e2);
+    if (f == 0) {
+        return false;
+    }
+
+    VectorCross3D(s, r.d, tmp);
+    double u = VectorDot3D(tmp, e2) / f;
+    VectorCross3D(e1, r.d, tmp);
+    double v = VectorDot3D(tmp, s) / f;
+    VectorCross3D(e1, s, tmp);
+    double t = - VectorDot3D(tmp, e2) / f;
+
+    if (!(u >= 0 && v >= 0 && u+v <= 1 && t > r.min_t && t < r.max_t && t < isect->t)) {
+        return false;
+    }
+
+    r.max_t = t;
+
+    isect->bsdfIndex = const_params.bsdfIndexes[primIndex];
+    isect->t = t;
+    isect->pIndex = primIndex;
+
+    float *n1 = normals;
+    float *n2 = normals + 3;
+    float *n3 = normals + 6;
+
+    float n[3];
+    for (int i = 0; i < 3; ++i)
+    {
+        n[i] = (1 - u - v) * n1[i] + u * n2[i] + v * n3[i];
+    }
+    if (VectorDot3D(r.d, n) > 0)
+    {
+        negVector3D(n, n);
+    }
+    readVector3D(n, isect->n);
+
+    return true;
+}
+
+__device__ bool sphereTest(int primIndex, GPURay& ray, double& t1, double& t2) {
+    float* primitive = const_params.positions + 9 * primIndex;
+    float* o = primitive;
+    float r = primitive[3];
+    float r2 = r * r;
+
+    float m[3];
+    subVector3D(o, ray.o, m);
+    double b = VectorDot3D(m, ray.d);
+    double c = VectorDot3D(m, m) - r2;
+    double delta = b * b - c;
+    if (delta < 0) {
+        return false;
+    }
+
+    t1 = b - sqrt(delta);
+    t2 = b + sqrt(delta);
+
+    if (t1 >= ray.max_t || t2 <= ray.min_t) {
+        return false;
+    }
+
+    return true;
+}
+
+__device__ bool sphereIntersect(int primIndex, GPURay& r) {
+    double tmp;
+    return sphereTest(primIndex, r, tmp, tmp);
+}
+
+__device__ bool sphereIntersect(int primIndex, GPURay& r, GPUIntersection *isect) {
+    double t1;
+    double t2;
+    bool res = sphereTest(primIndex, r, t1, t2);
+    if (!res) {
+        return false;
+    }
+    isect->bsdfIndex = const_params.bsdfIndexes[primIndex];
+    isect->pIndex = primIndex;
+
+    float* primitive = const_params.positions + 9 * primIndex;
+    float* o = primitive;
+    double t = t1;
+    if (t1 <= r.min_t) {
+        t = t2;
+    }
+    float n[3];
+    float tmp[3];
+    for (int i = 0; i < 3; ++i)
+    {
+        tmp[i] = r.d[i] * t;
+    }
+    addVector3D(r.o, tmp);
+    subVector3D(tmp, o, n);
+    normalize3D(n);
+    readVector3D(n, isect->n);
+    isect->t = t;
+    r.max_t = t;
+
+    return true;
+}
+
+__device__ bool intersect(int primIndex, GPURay& r) {
+    if (const_params.types[primIndex] == 0) {
+        // sphere
+        return sphereIntersect(primIndex, r);
+    } else {
+        // triangle
+        return triangleIntersect(primIndex, r);
+    }
+}
+
+__device__ bool intersect(int primIndex, GPURay& r, GPUIntersection *isect) {
+    if (const_params.types[primIndex] == 0) {
+        // sphere
+        return sphereIntersect(primIndex, r, isect);
+    } else {
+        // triangle
+        return triangleIntersect(primIndex, r, isect);
+    }
+}
