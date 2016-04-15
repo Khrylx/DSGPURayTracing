3daaba703a8a467f50bdc41b837cd4f890ecba57
Ken Ling <lingken.thu@icloud.com>
2016-04-14 22:52:50 -0400

    GlassBSDF sample_f; BSDF sample_f done

--- cuda_src/bsdf.cu
+++ cuda_src/bsdf.cu
@@ -64,14 +64,48 @@ __device__ float3 RefractionBSDF_sample_f(GPUBSDF *bsdf, const float *wo, float 
 		return make_float3(0.0, 0.0, 0.0);
 	}
 
-	double ni = bsdf->ior;
-	double no = 1;
+	float ni = bsdf->ior;
+	float no = 1;
 	if (wo[2] < 0) {
-		double tmp = ni;
+		float tmp = ni;
 		ni = no;
 		no = tmp;
 	}
-	double ratio = no / ni;
-	double coef = ratio * ratio / fmaxf(fabs(wi[2]), 1e-8);
+	float ratio = no / ni;
+	float coef = ratio * ratio / fmaxf(fabs(wi[2]), 1e-8);
 	return make_float3(bsdf->transmittance[0] * coef, bsdf->transmittance[1] * coef, bsdf->transmittance[2] * coef);
+}
+
+__device__ float3 GlassBSDF_sample_f(GPUBSDF *bsdf, const float *wo, float *wi, float *pdf, curandState *s) {
+	*pdf = 1;
+
+	bool res = refract(wo, wi, bsdf->ior);
+	if (!res) {
+		float coef = 1.0 / fmaxf(fabs(wi[2]), 1e-8);
+		return make_float3(bsdf->transmittance[0] * coef, bsdf->transmittance[1] * coef, bsdf->transmittance[2] * coef);
+	}
+
+	float ni = bsdf->ior;
+	float no = 1;
+	float cos_i = fabs(wi[2]);
+	float cos_o = fabs(wo[2]);
+	if (wo[2] < 0) {
+		float tmp = ni;
+		ni = no;
+		no = tmp;
+	}
+
+	float r1 = (no*cos_i - ni*cos_o)/(no*cos_i + ni*cos_o);
+    float r2 = (ni*cos_i - no*cos_o)/(ni*cos_i + no*cos_o);
+    float Fr = 0.5*(r1*r1 + r2*r2);
+
+    if (curand_uniform(s) <= Fr) {
+    	reflect(wo, wi);
+    	float coef = 1.0 / fmaxf(fabs(wi[2]), 1e-8);
+    	return make_float3(bsdf->reflectance[0] * coef, bsdf->reflectance[1] * coef, bsdf->reflectance[2] * coef);
+    } else {
+    	float ratio = no / ni;
+    	float coef = ratio * ratio / fmaxf(fabs(wi[2]), 1e-8);
+    	return make_float3(bsdf->transmittance[0] * coef, bsdf->transmittance[1] * coef, bsdf->transmittance[2] * coef);
+    }
 }
